199484b3f1a2450b188f64f931e306c9b8d7a170
Eve M <eve.proj.fun@gmail.com>
2022-07-01 15:10:22 +0800

    add match loop expression example

--- data-type-test/src/main.rs
+++ data-type-test/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unreachable_code)]
-
+#[warn(unused_labels)]
 fn main() {
     math();
     tup();
@@ -9,15 +9,18 @@ fn main() {
     let yy = {
         let x = 3;
         x + 1 // Expressions do not include ending semicolons
-        // If you add a semicolon to the end of an expression, you turn it into a statement, and it will then not return a value.
+              // If you add a semicolon to the end of an expression, you turn it into a statement, and it will then not return a value.
     };
     println!("The value of yy is: {}", yy);
     if_else();
     loop_forever();
     loop_number_range();
     loop_break_with_return();
+    loop_nesting_label();
     loop_while();
     loop_iterator();
+    match_number();
+    match_bool();
 }
 
 fn math() {
@@ -38,7 +41,6 @@ fn math() {
 
     let remainder = 43 % 5;
     println!("remainder {}", remainder);
-
 }
 
 fn tup() {
@@ -49,15 +51,13 @@ fn tup() {
     let (x, y, z) = tup;
     let (a, b, c) = (200, 5.4, 7);
 
-    println!("The value of z,y,x is: {} {} {}", z,y,x);
-    println!("The value of c,b,a is: {} {} {}", c,b,a);
+    println!("The value of z,y,x is: {} {} {}", z, y, x);
+    println!("The value of c,b,a is: {} {} {}", c, b, a);
     {
         let (x, y, z) = (a, b, c);
-        println!("inner - The value of z,y,x is: {} {} {}", z,y,x);
+        println!("inner - The value of z,y,x is: {} {} {}", z, y, x);
     }
-    println!("outer - The value of z,y,x is: {} {} {}", z,y,x);
-
-
+    println!("outer - The value of z,y,x is: {} {} {}", z, y, x);
 }
 
 fn tuple() {
@@ -87,7 +87,6 @@ fn array() {
     for n in array2 {
         println!("{}", n);
     }
-
 }
 fn if_else() {
     println!("--- if_else() ---");
@@ -101,19 +100,17 @@ fn if_else() {
         print!("{} is zero", n);
     }
 
-    let big_n =
-        if n < 10 && n > -10 {
-            println!(", and is a small number, increase ten-fold");
+    let big_n = if n < 10 && n > -10 {
+        println!(", and is a small number, increase ten-fold");
 
-            // This expression returns an `i32`.
-            10 * n
-        } else {
-            println!(", and is a big number, halve the number");
+        // This expression returns an `i32`.
+        10 * n
+    } else {
+        println!(", and is a big number, halve the number");
 
-            // This expression must return an `i32` as well.
-            n / 2
-            // TODO ^ Try suppressing this expression with a semicolon.
-        };
+        // This expression must return an `i32` as well.
+        n / 2 // Try suppressing this expression with a semicolon.
+    };
     //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
 
     println!("{} -> {}", n, big_n);
@@ -154,6 +151,7 @@ fn loop_nesting_label() {
     'outer: loop {
         println!("Entered the outer loop");
 
+        // need #[warn(unused_labels)]
         'inner: loop {
             println!("Entered the inner loop");
 
@@ -200,12 +198,14 @@ fn loop_while() {
 
 fn loop_iterator() {
     println!("--- loop_iterator() ---");
+    // by default the for loop will apply the into_iter function
 
+    /*
+     * iter()  like foreach
+     *  This borrows each element of the collection through each iteration.
+     *  Thus leaving the collection untouched and available for reuse after the loop
+     */
     let names = vec!["Bob", "Frank", "Ferris"];
-
-    /* iter()
-    *  This borrows each element of the collection through each iteration.
-    */ Thus leaving the collection untouched and available for reuse after the loop
     for name in names.iter() {
         match name {
             &"Ferris" => println!("There is a rustacean among us!"),
@@ -213,6 +213,63 @@ fn loop_iterator() {
             _ => println!("Hello {}", name),
         }
     }
+    println!("names: {:?}", names);
+
+    /*
+     * into_iter() like reduce
+     *  This borrows each element of the collection through each iteration.
+     *  Thus leaving the collection untouched and available for reuse after the loop
+     */
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    // println!("names: {:?}", names); // <--- will error
+
+    /*
+     * iter_mut() like map
+     *  This mutably borrows each element of the collection,
+     *  allowing for the collection to be modified in place.
+     */
+    let mut names = vec!["Bob", "Frank", "Ferris"]; // have to mut
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
 
     println!("names: {:?}", names);
+}
+fn match_number() {
+    println!("--- match_number() ---");
+    for number in 0..30 {
+        match number {
+            // Match a single value
+            1 => println!("{} is One!", number),
+            // Match several values
+            2 | 3 | 5 | 7 | 11 | 13 => println!("{} is a prime", number),
+            // Match an inclusive range
+            10..=19 => println!("{} is a teen", number),
+            // Handle the rest of cases
+            _ => println!("{} is not special", number), // if missing this line will error
+        }
+    }
+}
+
+fn match_bool() {
+    println!("--- match_bool() ---");
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1, // if missing this line will error
+    };
+
+    println!("{} -> {}", boolean, binary);
 }
